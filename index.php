5a38205b859245459a5e01522668f0f1bb8703fb
aerdnas <71413484+aerdnas@users.noreply.github.com>
2020-09-20 19:26:04 +0200

    Update index.php: removed for statement

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <?php
 
- for ($i = 10; $i >= 1; $i--) {
-   echo $i;
-   sleep(10);               //two seconds delay
-}
- 
+ sleep(120);                //two seconds delay
  $URL = getenv('URL');      //get url stored into config var
  header("Location: $URL");  //open new url (redirect)
  exit;

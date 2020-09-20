f1eaf47b77baa31e427ef5fc36ec5edda0480927
aerdnas <71413484+aerdnas@users.noreply.github.com>
2020-09-20 12:12:15 +0200

    Update index.php deploy error solving #1

--- index.php
+++ index.php
@@ -8,9 +8,9 @@
 </html>
 <?php
 
- sleep(120);                //two seconds delay
+ sleep(10);                //two seconds delay
  $URL = getenv('URL');      //get url stored into config var
- header("Location: $URL");  //open new url (redirect)
+ header('Location: $URL');  //open new url (redirect)
  exit;
 
 ?>

437f619adcbe6395104fc52896644d8a4009d5a7
aerdnas <71413484+aerdnas@users.noreply.github.com>
2020-09-21 01:01:11 +0200

    Removed conf functions

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 
- set_time_limit(300);             //set 125 seconds max execution time 
  sleep(120);                         //wait two seconds
  $URL = getenv('URL');               //get url stored into config var
  header("Location: $URL");           //open new url (redirect)

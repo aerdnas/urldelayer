7bb373c2f728bcff1691e81b2c914e54bf00196f
aerdnas <71413484+aerdnas@users.noreply.github.com>
2020-09-20 12:25:39 +0200

    Update index.php: delay adjustment #2

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
- sleep(60);                //two seconds delay
+ sleep(30);                //two seconds delay
  $URL = getenv('URL');      //get url stored into config var
  header("Location: $URL");  //open new url (redirect)
  exit;

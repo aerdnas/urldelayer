635e83b085db7701e149b29c839635213da4953a
aerdnas <71413484+aerdnas@users.noreply.github.com>
2020-09-21 00:54:18 +0200

    Added set_time_limit

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
- ini_set('max_execution_time', 125); //set 125 seconds max execution time 
+ set_time_limit ( 300 );             //set 125 seconds max execution time 
  sleep(120);                         //wait two seconds
  $URL = getenv('URL');               //get url stored into config var
  header("Location: $URL");           //open new url (redirect)

1ffb4b9de3ce8a6abcc29e433dbf67464cebd1c8
aerdnas <71413484+aerdnas@users.noreply.github.com>
2020-09-20 12:35:14 +0200

    Update index.php with new for statement for delay control

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 
- sleep(10);                //two seconds delay
+ for ($i = 10; $i <= 1; $i--) {
+   echo $i;
+   sleep(10);               //two seconds delay
+}
+ 
  $URL = getenv('URL');      //get url stored into config var
  header("Location: $URL");  //open new url (redirect)
  exit;

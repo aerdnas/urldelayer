f2fde9aef0dd54559e96fff85dfaa024cf022d5b
aerdnas <71413484+aerdnas@users.noreply.github.com>
2020-09-21 00:33:52 +0200

    Added ini_set() to prevent deploy error

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 
- sleep(120);                //two seconds delay
- $URL = getenv('URL');      //get url stored into config var
- header("Location: $URL");  //open new url (redirect)
+ ini_set('max_execution_time', 125) //set 125 seconds max execution time 
+ sleep(120);                        //wait two seconds
+ $URL = getenv('URL');              //get url stored into config var
+ header("Location: $URL");          //open new url (redirect)
  exit;
 
 ?>

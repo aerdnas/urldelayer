12f355338881681c106ac98c1129c09dbd40561a
aerdnas <71413484+aerdnas@users.noreply.github.com>
2020-09-23 00:14:41 +0200

    Error fixing

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
 <?php
 
- $DELAY = 3;
- echo $DELAY;
- $DELAY = $_GET['delay'];            //GET parameter test
- echo $DELAY;
- sleep(25);                          //wait 25 seconds
- $URL = getenv('URL');               //get url stored into config var
+ $DELAY = $_GET['delay'];                     //GET parameter test
+ sleep(25);                                   //wait 25 seconds
+ $URL = getenv('URL');                        //get url stored into config var
  $DELAY--;
- header("Location: index.php?delay=$DELAY");           //open new url (redirect)
+ header("Location: index.php?delay=$DELAY");  //open new url (redirect)
  exit;
 
 ?>

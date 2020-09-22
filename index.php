89d934caac7d41700366f2ad662911ce3248c2a5
aerdnas <71413484+aerdnas@users.noreply.github.com>
2020-09-22 23:37:05 +0200

    GET method test

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
- sleep(120);                         //wait two seconds
+ echo $_GET['delay'];                //GET parameter test
+ sleep(25);                          //wait 25 seconds
  $URL = getenv('URL');               //get url stored into config var
  header("Location: $URL");           //open new url (redirect)
  exit;

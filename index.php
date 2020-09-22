3c65ea9b86aa0daf45ee5b2d3ee0868fa6db917b
aerdnas <71413484+aerdnas@users.noreply.github.com>
2020-09-23 00:09:05 +0200

    Corrected mistakes

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
 
  $DELAY = 3;
+ echo $DELAY;
  $DELAY = $_GET['delay'];            //GET parameter test
  echo $DELAY;
  sleep(25);                          //wait 25 seconds
  $URL = getenv('URL');               //get url stored into config var
- $DELAY = $DELAY - 1;
- header("Location: index.php?$DELAY");           //open new url (redirect)
+ $DELAY--;
+ header("Location: index.php?delay=$DELAY");           //open new url (redirect)
  exit;
 
 ?>

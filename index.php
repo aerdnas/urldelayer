07538ec96c32322f0fdfb57e8bd247bdf03a0715
aerdnas <71413484+aerdnas@users.noreply.github.com>
2020-09-28 00:35:18 +0200

    deleted old code - made in line with v1.1

--- index.php
+++ index.php
@@ -5,12 +5,9 @@
  $DELAY--;                                    //decrease the number of delays
  if($DELAY==0){                               //if all the delays are elapsed
    $URL = getenv('URL');                      //get url stored into config var
-   //header("Location: $URL");}               //open new url (redirect)
    file_get_contents($URL);}                  //open new url (new connection)
  else {                                       //else continue elapsing delays
-   //header("Location: index.php?delay=$DELAY");}//recursive call to elapse a new delay
    $RE_URL = "https://urldelayer.herokuapp.com/index.php?delay=".$DELAY;//make new url for recursive connection 
    file_get_contents($RE_URL);}               //recursive connection to elapse a new delay
- //exit;
 
 ?>

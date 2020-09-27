ca80f2cbbad90bbf1bdff6b04f12fe07a4e88a46
aerdnas <71413484+aerdnas@users.noreply.github.com>
2020-09-27 23:55:10 +0200

    code indentation adjustment

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  $DELAY--;                                    //decrease the number of delays
  if($DELAY==0){                               //if all the delays are elapsed
    $URL = getenv('URL');                      //get url stored into config var
-   header("Location: $URL");}                  //open new url (redirect)
+   header("Location: $URL");}                 //open new url (redirect)
  else                                         //else continue elapsing delays
    header("Location: index.php?delay=$DELAY");//recursive call to elapse a new delay
  exit;
